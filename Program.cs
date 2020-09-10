25a63083b5909dc07f196c9db7683962fabb75c6
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-10 23:43:55 +0700

    chuyen doi tien te

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace chuyendoitiente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("CT CHUYỂN ĐỔI TIỀN TỆ USD -> VNĐ");
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Nhập tỉ giá tiền (Ví dụ 23000 Vnđ/USD):");
+            int tigia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập USD :");
+            int usd = int.Parse(Console.ReadLine());
+            doitien(tigia, usd);
+        }
+        static int doitien(int tigia, int usd)
+        {
+            int vnd = tigia * usd;
+            Console.WriteLine("USD => VNĐ là : {0} Vnđ", vnd);
+            return vnd;
+        }
+    }
+}
